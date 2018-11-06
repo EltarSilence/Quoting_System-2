340ae78eab814cae2afa4f0c5dd0b0ee5abbb21f
Giovanni Rizza <rizza67jorge@gmail.com>
2018-11-06 13:28:01 +0100

    Profilo progressivo 1

--- resources/views/my-bet.blade.php
+++ resources/views/my-bet.blade.php
@@ -4,9 +4,23 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-4">
+
           @for($i=0; $i<count($userBets); $i++)
-            <div>
-              {!! $userBets[$i] !!}
+          {!! $userBets[$i] !!}
+
+            <div class="card" style="width: 18rem;">
+              <div class="card-header">
+                Scommessa n. {!! $userBets[$i]['idS'] !!}
+                <h6>
+                  Puntata:
+                  <span class="badge badge-primary">
+                    {!! $userBets[$i]['coinS'] !!} exacoins
+                  </span>
+                </h6>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item"></li>
+              </ul>
             </div>
           @endfor
         </div>
