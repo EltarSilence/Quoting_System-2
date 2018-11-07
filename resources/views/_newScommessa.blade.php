22b6827c85a30b98ae90f52e4d222207b9597060
Zexal0807 <gallinar00@gmail.com>
2018-11-07 08:41:24 +0100

    a

--- resources/views/_newScommessa.blade.php
+++ resources/views/_newScommessa.blade.php
@@ -2,6 +2,27 @@
 <span>ACCEDI X GICARE</span>
 @else
 <div class="col-md-12" id="newScommessa" ver="">
-  <div class="col-xs-12"></div>
+
+  <div class="col-xs-12 alert alert-primary">
+    <h3>MT_0001.json</h3>
+  </div>
+  <div class="col-xs-12 alert">
+    <h3>MT_0001.json</h3>
+  </div>
+
+
+
+  <center>
+
+  <div class="col-xs-12">
+    Breve descrizione, se c'è ... . . ..... ... .. ...............
+
+    <button class="btn btn-primary col-xs-12" type="button" data-toggle="collapse" data-target="#c'.$s.'" aria-expanded="false">
+    </button>
+    <div class="collapse row" id="c'.$s.'" style="margin:0px">
+    </div>
+
+  </div>
+</center>
 </div>
 @endguest
