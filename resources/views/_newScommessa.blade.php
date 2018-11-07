d949f90f237a795b4a8ff8bc1d624a646549ed9b
Zexal0807 <gallinar00@gmail.com>
2018-11-07 10:54:37 +0100

    33 trentini entrano a Trento trotterellando

--- resources/views/_newScommessa.blade.php
+++ resources/views/_newScommessa.blade.php
@@ -3,26 +3,57 @@
 @else
 <div class="col-md-12" id="newScommessa" ver="">
 
-  <div class="col-xs-12 alert alert-primary">
-    <h3>MT_0001.json</h3>
-  </div>
-  <div class="col-xs-12 alert">
-    <h3>MT_0001.json</h3>
-  </div>
+  EUO
+  <div class="card">
+    <div class="card-body">
+      <button class="btn btn-primary">
+        Back
+      </button>
+      Titolo se c'è
 
+      <div class="card">
+        <div class="btn card-header" data-toggle="collapse" data-target="#collapseOne">
+          <h5 class="m-0">
+              Gallina
+          </h5>
+        </div>
+        <div id="collapseOne" class="collapse">
+          <div class="card-body row pt-0 pb-1">
+            <div class="col-sm-4">
+              <div class="intestazione">VOTO ESATTO</div>
 
+              <div class="row click">
+                <div class="col-6 font-weight-bold">10</div>
+                <div class="col-6">1.56</div>
+                <input type="radio" name="'.$key.'" data-type="E" data-value="'.$k.'" data-quote="'.round($v, 2).'"/>
+              </div>
 
-  <center>
+            </div>
+            <div class="col-sm-4">
+              <div class="intestazione">UNDER</div>
 
-  <div class="col-xs-12">
-    Breve descrizione, se c'è ... . . ..... ... .. ...............
+              <div class="row click">
+                <div class="col-6 font-weight-bold">6.25</div>
+                <div class="col-6">1.56</div>
+                <input type="radio" name="'.$key.'" data-type="E" data-value="'.$k.'" data-quote="'.round($v, 2).'"/>
+              </div>
 
-    <button class="btn btn-primary col-xs-12" type="button" data-toggle="collapse" data-target="#c'.$s.'" aria-expanded="false">
-    </button>
-    <div class="collapse row" id="c'.$s.'" style="margin:0px">
+            </div>
+            <div class="col-sm-4">
+              <div class="intestazione">OVER</div>
+
+              <div class="row click">
+                <div class="col-6 font-weight-bold">6.25</div>
+                <div class="col-6">1.56</div>
+                <input type="radio" name="'.$key.'" data-type="E" data-value="'.$k.'" data-quote="'.round($v, 2).'"/>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-
   </div>
-</center>
 </div>
 @endguest
