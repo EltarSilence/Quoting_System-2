f63380a9f91f7421ccf1e8958e6b39e2e6a5aba2
unknown <gallinar00@gmail.com>
2018-11-08 18:38:28 +0100

    a

--- public/js/_newScommessa.js
+++ public/js/_newScommessa.js
@@ -37,43 +37,42 @@ function setVerifica(){
 	$.ajax({
 		url : "/getScommessa",
 		type : "POST",
-		data : {'scommessa': $('#newScommessa').attr('ver')},
+		data : {'scommessa': "SN_1_1"},
+		//data : {'scommessa': $('#newScommessa').attr('ver')},
 		success : function(data){
-			switch (data['type']) {
-				case "EUO":
-
-					break;
-				case "SN":
-
-					break;
-				case "MT":
-
-					break;
-				default:
-					break;
-			}
-
-			html = '';
-
-
-
-			debugger;
-			/*
-			for(var i = 0; i < data.length; i++){
-				let chiave = data[i]['fileD'];
-				html += '<button class="btn btn-primary verifica" id="'+chiave+'"> - Verifica di ... </button>';
-			}
+			html = '<div class="card"><div class="card-body"><div class="mb-2 ml-0 mr-0 row"><button class="btn btn-primary col-2">Back</button></div>';
+			html += getHtml(data);
+			html += '</div></div>';
 			$('#newScommessa').html(html);
-			$(".verifica").on('click', function(){
-				$('#newScommessa').attr('ver', $(this).attr('id'));
-				setVerifica();
-			});*/
 		},
 		error : function(e){
 			debugger;
 		}
 	});
 
+	function getHtml(data){
+		html = '';
+		switch (data['type']) {
+			case "EUO":
+
+				break;
+			case "SN":
+				for(let i = 0; i < data['file'].length; i++){
+					html += '<div class="card mt-1"><div class="btn card-header" data-toggle="collapse" data-target="#' + data['filename'] + '_' + i + '"><h5 class="m-0">' + data['file'][i]['descrizione'] + '</h5></div><div id="' + data['filename'] + '_' + i + '" class="collapse row pl-2 pr-2"><div class="card-body col-6 pt-0 pb-1 border-right"><div class="intestazione">SI</div><div class="click"><div>' + data['file'][i]['SI'] + '</div><input type="radio" name="' + data['filename'] + '_' + i + '" data-value="SI" data-quote="' + data['file'][i]['SI'] + '"/></div></div><div class="card-body col-6 pt-0 pb-1 border-left"><div class="intestazione">NO</div><div class="click"><div>' + data['file'][i]['NO'] + '</div><input type="radio" name="' + data['filename'] + '_' + i + '" data-value="NO" data-quote="' + data['file'][i]['NO'] + '"/></div></div></div></div>';
+				}
+				break;
+			case "MT":
+				html += '<div class="card"><div class="btn card-header"><h5 class="m-0">' + data['file']['descrizione'] + '</h5></div>';
+				for(let i = 0; i < Object.keys(data['file']).length - 1; i++){
+					html += '<div class="card-body row ml-0 mr-0 border pt-1 pb-1"><div class="intestazione col-6">' + data['file'][i]['titolo'] + '</div><div class="click col-6"><div>' + data['file'][i]['quota'] + '</div><input type="radio" name="' + data['filename'] + '" data-value="' + i + '" data-quote="' + data['file'][i]['quota'] + '"/></div></div>';
+				}
+				html += '</div>';
+				break;
+			default:
+				break;
+		}
+		return html;
+	}
 
 
 }
