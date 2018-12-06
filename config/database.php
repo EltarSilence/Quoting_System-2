c853c01d4028cf12decca29dcc5c5628e37f99ed
Giovanni Rizza <eltarsilence@gmail.com>
2018-12-06 23:31:58 +0100

    Test 1

--- config/database.php
+++ config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'quoting2'),
-            'username' => env('DB_USERNAME', 'root'),
+            'host' => env('DB_HOST', 'https://phplaravel-224769-684695.cloudwaysapps.com'),
+            'port' => env('DB_PORT', '8082'),
+            'database' => env('DB_DATABASE', 'ducpmvvnzf'),
+            'username' => env('DB_USERNAME', 'ducpmvvnzf'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
